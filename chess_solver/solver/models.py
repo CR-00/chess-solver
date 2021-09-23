d3802f27ad6ba304263a82bcb35902e397de6f7d
Chris <cjr022@student.bham.ac.uk>
2021-09-23 22:40:09 +0100

    Removed model

--- chess_solver/solver/models.py
+++ chess_solver/solver/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
 
-class ChessGame(models.Model):
-    """
-
-    The state of the game is stored as a string representation in the database.
-
-    For the front end representation the JS library 'chessboard.js' is used:
-    https://chessboardjs.com/index.html
-
-    For the backend we use the Python library python-chess:
-    https://python-chess.readthedocs.io/en/latest/
-
-    Luckily both log the state of the game in the same ASCII format (epd) so are compatible
-    with one another.
-
-    """
-
--- chess_solver/solver/tests.py
+++ chess_solver/solver/tests.py
@@ -1,6 +1,1 @@
 from django.test import TestCase
-import chess
-import os
-
-board = chess.Board(fen=chess.STARTING_FEN)
-print(chess.STARTING_BOARD_FEN)
